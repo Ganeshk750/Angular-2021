164cf0567815f28f0db109bfec2c03e432020334
Ganeshk750 <ganesh9ot@gmail.com>
2021-01-05 12:04:08 +0530

    adding new UI With Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PasswordGeneratorComponent } from './apps/password-generator/password-generator.component';
+
+const routes: Routes = [
+  { path: '', redirectTo:"/", pathMatch: "full" },
+  { path: 'pw', component: PasswordGeneratorComponent },
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   declarations: []
+  
 })
 export class AppRoutingModule { }
