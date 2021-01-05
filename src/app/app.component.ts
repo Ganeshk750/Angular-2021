bb7f24bcd4fc1a81f62eabaf4b5026a1cbe9d50e
Ganeshk750 <ganesh9ot@gmail.com>
2021-01-05 11:16:41 +0530

    changing design of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- // title = 'Password Generator App';
+  // title = 'Password Generator App';
 
   length = 0;
   includeLetters = false;
@@ -57,4 +57,5 @@ export class AppComponent {
     }
     this.password = generatedPassword;
   }
+
 }
