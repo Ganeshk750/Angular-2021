fbdf862d1d5f10efa714f1450286ea4f92ded495
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-03-19 17:34:21 +0530

    # adding dependency for chart and multiple graph component with seprate module

---
+++ src/app/chart/components/bubble-graph/bubble-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-graph',
+  templateUrl: './bubble-graph.component.html',
+  styleUrls: ['./bubble-graph.component.css']
+})
+export class BubbleGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
